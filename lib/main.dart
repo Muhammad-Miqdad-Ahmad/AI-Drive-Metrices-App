5015494c107b59f9afbb52892c6cff0130e590e3
Maheen <zmaheen05@gmail.com>
2026-06-09 11:27:42 +0500

    supabase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: 'https://gsgmyvdszgejcadgrvgs.supabase.co/rest/v1/',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzZ215dmRzemdlamNhZGdydmdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MTk3NTIsImV4cCI6MjA5NjQ5NTc1Mn0.2IJohd6T1WK1QpXD_yvWHpTaCmh6U30qrgeMbzVQYys',
+  );
 
   // Lock to portrait
   await SystemChrome.setPreferredOrientations([
